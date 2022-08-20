5fc9f58d344f39cf5323b474e7bb84dc4ecdaeae
Viktoria Berenshtein <vikel-rostov@mail.ru>
2022-08-20 17:47:52 +0300

    solving task from codewars

--- 6-kyu/ArrayDiff.js
+++ 6-kyu/ArrayDiff.js
@@ -9,7 +9,16 @@ function arrayDifference(arr1, arr2) {
     return arr1;
 }
 
+function arrayDifferenceWithSet(arr1, arr2) {
+    let set = new Set(arr1);
+    for (const item of arr2) {
+        set.delete(item);
+    }
+    return arr1;
+}
+
 let arr1 = [1, 2, 2, 2, 3, 2, 2, 1];
 let arr2 = [2];
 
 console.log(arrayDifference(arr1, arr2));
+console.log(arrayDifferenceWithSet(arr1, arr2));
